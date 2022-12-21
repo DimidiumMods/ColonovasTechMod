4d7b7ca817a42f21aee8d91b88e23a754dfa421c
LegionModding <legion@legionmodding.com>
2022-12-21 15:17:52 +0000

    Hide rubber durability bar

--- src/main/java/net/colonova/colonovastechmod/item/ItemRubber.java
+++ src/main/java/net/colonova/colonovastechmod/item/ItemRubber.java
@@ -2,11 +2,18 @@ package net.colonova.colonovastechmod.item;
 
 import net.colonova.colonovascore.api.item.ItemBase;
 import net.colonova.colonovastechmod.util.Reference;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ItemRubber extends ItemBase
 {
     public ItemRubber()
     {
-        super(Reference.CREATIVE_TAB_ITEMS, 64, true);
+        super(Reference.CREATIVE_TAB_ITEMS, 64, false);
     }
 }
